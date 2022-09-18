8cf0b487a6b7cac5c593d17333db9745f0c2a797
Andre Crispim <andrecrispim41@gmail.com>
2022-09-18 10:33:49 -0300

    ProgAvançado

---
+++ src/Exercicio10.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio10 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int a, anos, meses, dias;
+
+		a = input.nextInt();
+
+		anos = a / 365;
+		meses = (a % 365) / 30;
+		dias = (a % 365) % 30;
+		System.out.println(anos + " ano(s)");
+		System.out.println(meses + " mes(es)");
+		System.out.println(dias + " dia(s)");
+
+	}
+
+}
